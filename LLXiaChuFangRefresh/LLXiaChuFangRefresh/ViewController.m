958a94a377b42742336ad2773fbd8f3e26cff634
lilongcnchome <lilongcnc@vip.qq.com>
2016-04-07 22:55:24 +0800

    删除 gif图片太大了,替换成小的 gif 演示图片

--- LLXiaChuFangRefresh/LLXiaChuFangRefresh/ViewController.m
+++ LLXiaChuFangRefresh/LLXiaChuFangRefresh/ViewController.m
@@ -8,15 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>{
+    CGFloat screenWidth;
+}
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,strong) UIView *refreshView;
 @end
 
 @implementation ViewController
 
+static NSString *const CellID = @"CellID";
+static CGFloat const tableViewCellHeight = 60.f;
+static int const tableViewRowOfNumber= 6;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    screenWidth = [UIScreen mainScreen].bounds.size.width;
+    
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellID];
+    _tableView.allowsSelection = NO;
+    _tableView.tableFooterView = [UIView new];
+
+    _refreshView = ({
+        CGFloat refreshViewHeight = 40;
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetMinY(_tableView.frame), screenWidth, refreshViewHeight)];
+        view.backgroundColor = [UIColor brownColor];
+        
+        [self.view addSubview:view];
+        view;
+    
+    });
+    
+}
+
+
+
+#pragma mark UItableViewDelegate && UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return tableViewRowOfNumber;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellID forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"LeftTableView:%zd",indexPath.row];
+    cell.backgroundColor = [UIColor orangeColor];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return tableViewCellHeight;
+}
+
+
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"LeftTableView 点击了 %zd-------%zd",indexPath.section,indexPath.row);
+
 }
 
 - (void)didReceiveMemoryWarning {
